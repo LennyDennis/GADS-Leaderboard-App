0b8b4fc1d339a3d4080fd82763a5912375a5d416
LennyDennis <lennydennis@gmail.com>
2020-09-08 10:40:35 +0300

    Add a handler to navigate to the Main Activity

--- app/src/main/java/com/lennydennis/aadpracticeproject/SpashScreen.java
+++ app/src/main/java/com/lennydennis/aadpracticeproject/SpashScreen.java
@@ -2,7 +2,9 @@ package com.lennydennis.aadpracticeproject;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
 import android.view.WindowManager;
 import android.view.animation.Animation;
 import android.view.animation.AnimationUtils;
@@ -10,6 +12,8 @@ import android.view.animation.AnimationUtils;
 import com.lennydennis.aadpracticeproject.databinding.ActivitySpashScreenBinding;
 
 public class SpashScreen extends AppCompatActivity {
+
+    private static final int SPLASH_DURATION = 3000;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,6 +24,14 @@ public class SpashScreen extends AppCompatActivity {
 
         Animation topAnimation = AnimationUtils.loadAnimation(this, R.anim.top_animation);
         splashScreenBinding.gadsLogo.setAnimation(topAnimation);
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent intent = new Intent(SpashScreen.this,MainActivity.class);
+                startActivity(intent);
+            }
+        }, SPLASH_DURATION);
 
     }
 }
