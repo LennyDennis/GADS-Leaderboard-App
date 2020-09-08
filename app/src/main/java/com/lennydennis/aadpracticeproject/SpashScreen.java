a6ef4f3f6bca927a8aec0cf3d55605af0e13e68f
LennyDennis <lennydennis@gmail.com>
2020-09-08 09:53:19 +0300

    Create a splash screen

---
+++ app/src/main/java/com/lennydennis/aadpracticeproject/SpashScreen.java
@@ -0,0 +1,14 @@
+package com.lennydennis.aadpracticeproject;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class SpashScreen extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_spash_screen);
+    }
+}
