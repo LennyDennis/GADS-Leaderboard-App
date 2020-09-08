d83faa7fafbf97c333d366a71d0ac62f39144b62
LennyDennis <lennydennis@gmail.com>
2020-09-08 17:00:51 +0300

    Create the LeaderboardAPI interface

--- app/src/main/java/com/lennydennis/aadpracticeproject/api/LeaderboardApi.java
+++ app/src/main/java/com/lennydennis/aadpracticeproject/api/LeaderboardApi.java
@@ -1,4 +1,19 @@
 package com.lennydennis.aadpracticeproject.api;
 
+import com.lennydennis.aadpracticeproject.model.LearningHours;
+import com.lennydennis.aadpracticeproject.model.SkillIQScores;
+
+import java.util.List;
+
+import retrofit2.Call;
+import retrofit2.http.GET;
+
 public interface LeaderboardApi {
+
+    @GET("/api/hours")
+    Call<List<LearningHours>> getLearningHours();
+
+    @GET("/api/skilliq")
+    Call<List<SkillIQScores>> getSkillIQscores();
+
 }
