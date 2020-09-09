554a12818d1baec6a333bdf3bdcd6ff6dc3d1c00
LennyDennis <lennydennis@gmail.com>
2020-09-09 12:36:11 +0300

    Bind the skill iq item view with their data

--- app/src/main/java/com/lennydennis/aadpracticeproject/adapter/SkillIQLeadersAdapter.java
+++ app/src/main/java/com/lennydennis/aadpracticeproject/adapter/SkillIQLeadersAdapter.java
@@ -1,33 +1,59 @@
 package com.lennydennis.aadpracticeproject.adapter;
 
+import android.content.Context;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
 
+import com.lennydennis.aadpracticeproject.databinding.SkillIqItemBinding;
+import com.lennydennis.aadpracticeproject.model.SkillIQScoresLeaders;
+
+import java.util.ArrayList;
+
 public class SkillIQLeadersAdapter extends RecyclerView.Adapter<SkillIQLeadersAdapter.ViewHolder> {
+
+    private Context mContext;
+    private ArrayList<SkillIQScoresLeaders> mSkillIQScoresLeaders;
+
+    public SkillIQLeadersAdapter(Context context, ArrayList<SkillIQScoresLeaders> skillIQScoresLeaders) {
+        mContext = context;
+        mSkillIQScoresLeaders = skillIQScoresLeaders;
+    }
 
     @NonNull
     @Override
     public ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
-        return null;
+        LayoutInflater layoutInflater = LayoutInflater.from(parent.getContext());
+        SkillIqItemBinding skillIqItemBinding = SkillIqItemBinding.inflate(layoutInflater,parent,false);
+        return new ViewHolder(skillIqItemBinding);
     }
 
     @Override
     public void onBindViewHolder(@NonNull ViewHolder holder, int position) {
-
+        SkillIQScoresLeaders skillIQScoresLeader = mSkillIQScoresLeaders.get(position);
+        holder.skillIQBinding(skillIQScoresLeader);
     }
 
     @Override
     public int getItemCount() {
-        return 0;
+        return mSkillIQScoresLeaders.size();
     }
 
     public class ViewHolder extends RecyclerView.ViewHolder{
 
-        public ViewHolder(@NonNull View itemView) {
-            super(itemView);
+        public SkillIqItemBinding mSkillIqItemBinding;
+
+        public ViewHolder(@NonNull SkillIqItemBinding skillIqItemBinding) {
+            super(skillIqItemBinding.getRoot());
+            mSkillIqItemBinding = skillIqItemBinding;
+        }
+
+        public void skillIQBinding(SkillIQScoresLeaders skillIQScoresLeaders){
+            mSkillIqItemBinding.setSkillIQLeaders(skillIQScoresLeaders);
+            mSkillIqItemBinding.executePendingBindings();
         }
     }
 }
