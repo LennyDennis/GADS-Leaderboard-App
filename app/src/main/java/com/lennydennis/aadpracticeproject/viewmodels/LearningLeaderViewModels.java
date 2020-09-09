f3b8fccb7f3a3043a6ba2b3d03219ddf326d39b2
LennyDennis <lennydennis@gmail.com>
2020-09-09 11:03:02 +0300

    Connect the LearningLeadersViewModel to the repository that makes the network call

--- app/src/main/java/com/lennydennis/aadpracticeproject/viewmodels/LearningLeaderViewModels.java
+++ app/src/main/java/com/lennydennis/aadpracticeproject/viewmodels/LearningLeaderViewModels.java
@@ -1,7 +1,30 @@
 package com.lennydennis.aadpracticeproject.viewmodels;
 
+import android.app.Application;
+
+import androidx.annotation.NonNull;
+import androidx.lifecycle.AndroidViewModel;
+import androidx.lifecycle.LiveData;
 import androidx.lifecycle.ViewModel;
 
-public class LearningLeaderViewModels extends ViewModel {
-    // TODO: Implement the ViewModel
+import com.lennydennis.aadpracticeproject.model.LearningHoursLeaders;
+import com.lennydennis.aadpracticeproject.repository.LeaderBoardRepository;
+
+import java.util.List;
+
+public class LearningLeaderViewModels extends AndroidViewModel {
+    private LeaderBoardRepository mLeaderBoardRepository;
+    private LiveData<List<LearningHoursLeaders>> mLearningHoursLeaders;
+
+
+    public LearningLeaderViewModels(@NonNull Application application) {
+        super(application);
+
+        mLeaderBoardRepository = new LeaderBoardRepository();
+        mLearningHoursLeaders = mLeaderBoardRepository.getLearningHoursLeaders();
+    }
+
+    public LiveData<List<LearningHoursLeaders>> getLearningHoursLeaders(){
+        return mLearningHoursLeaders;
+    }
 }
